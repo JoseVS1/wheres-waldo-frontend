2e9c4e31824e61da1bacfdd7c39f2f61c9f598ba
JoseVS1 <105169283+JoseVS1@users.noreply.github.com>
2025-02-18 21:00:06 -0600

    Update app title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export const Navbar = ({stopwatchIsRunning, time, setTime}) => {
   return (
     <nav>
         <ul>
-            <li className="title" onClick={() => window.location.reload()}><h1>Where's Waldo</h1></li>
+            <li className="title" onClick={() => window.location.reload()}><h1>Where's Waldo?</h1></li>
             <li><Stopwatch stopwatchIsRunning={stopwatchIsRunning} time={time} setTime={setTime} /></li>
         </ul>
     </nav>
